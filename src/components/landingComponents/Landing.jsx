7642268d6aa4603edea4a90779b62a2a1afa8963
SWELLZ <microsoftdavid4@gmail.com>
2022-10-13 12:24:12 -0700

    add fetched data displayed

--- src/components/landingComponents/Landing.jsx
+++ src/components/landingComponents/Landing.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 const Landing = () => {
+  const clientId = `${process.env.REACT_APP_CLIENT_ID}`
+  const redirectUri = `${process.env.REACT_APP_REDIRECT_URI}`
+  const endpoint = `${process.env.REACT_APP_AUTH_ENDPOINT}`
+  const responseType = `${process.env.REACT_APP_RESPONSE_TYPE}`
+
   return (
     <div className="w-full h-screen flex flex-col justify-center bg-black-500">
         <div className='text-white flex flex-col gap-6 text-center items-center'>
             <h1 className='text-4xl font-bold'>SPOTIFY SHUFFLE</h1>
             <p className='w-1/2 text-center'>Let's face it, the built in shuffle feature in Spotify is terrible (no offense). That's why Spotify Shuffle is here; All you need to do is sign in and choose a playlist, we will take care of the rest and seemlessly rearrange your playlist for</p>
             <div>
-                <button className='bg-green-500 rounded-full p-3 hover:bg-green-600 font-bold'>CONNECT SPOTIFY</button>
+                <a href={`${endpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}`} className='bg-green-500 rounded-full p-4 hover:bg-green-600 font-bold flex align-center'>CONNECT SPOTIFY</a>
             </div>
         </div>
     </div>
--- src/components/shuffleComponents/Shuffle.jsx
+++ src/components/shuffleComponents/Shuffle.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Shuffle = () => {
-  return (
-    <div>Shuffle</div>
-  )
+    const [token, setToken] = useState('');
+    const [playlists, setPlaylists] = useState({});
+    
+    useEffect(() => {
+        //variables for access token
+        var access_token;
+        const hash = window.location.hash;
+
+        //Extract token from url
+        if (hash.includes('access_token')){
+            access_token = hash.split('&').find(elem => elem.startsWith('#access_token')).split('=')[1];
+        }
+        setToken(access_token);
+    }, [])
+
+    const fetchPlaylists = async () => {
+        await fetch('https://api.spotify.com/v1/me/playlists', {
+            headers: {'Authorization': 'Bearer ' + token}
+        }).then(response => response.json())
+        .then(data => setPlaylists(data.items))
+        .catch(e => console.error(e))
+    }
+
+    useEffect(() => {
+        fetchPlaylists();
+    }, [token])
+    useEffect(() => {
+        console.log(playlists)
+    }, [playlists])
+
+    return (
+        <>
+        {playlists ?
+            <div className='flex bg-black-500 text-white flex-wrap'> 
+                {playlists.map((item, i) => (
+                    <button key={i}>
+                        <img className='w-48 h-48' src={item.images[0].url} alt='playlist cover' />
+                        <h3 className='font-bold'>{item.name}</h3>
+                    </button>
+                ))}
+            </div>
+            :
+            <div>
+                <p>null</p>
+            </div>
+        }
+        </>
+    )
 }
 
 export default Shuffle
