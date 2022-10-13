11aeeec102e6cca16444b0d81698f18623037327
SWELLZ <microsoftdavid4@gmail.com>
2022-10-13 14:35:13 -0700

    display playlists to screen

--- src/components/shuffleComponents/Shuffle.jsx
+++ src/components/shuffleComponents/Shuffle.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 const Shuffle = () => {
     const [token, setToken] = useState('');
-    const [playlists, setPlaylists] = useState({});
+    const [playlists, setPlaylists] = useState([]);
     
     useEffect(() => {
         //variables for access token
@@ -27,27 +27,21 @@ const Shuffle = () => {
     useEffect(() => {
         fetchPlaylists();
     }, [token])
-    useEffect(() => {
-        console.log(playlists)
-    }, [playlists])
 
     return (
-        <>
-        {playlists ?
-            <div className='flex bg-black-500 text-white flex-wrap'> 
+        <div className='bg-black-500 pt-7 min-h-screen p-5'>
+        <h1 className='text-white text-center text-2xl font-bold'>Choose your playlist</h1>
+        {playlists &&
+            <div className='grid bg-black-500 text-white grid-cols-3 gap-4 place-content-center mt-7'> 
                 {playlists.map((item, i) => (
-                    <button key={i}>
-                        <img className='w-48 h-48' src={item.images[0].url} alt='playlist cover' />
-                        <h3 className='font-bold'>{item.name}</h3>
+                    <button key={i} className='flex items-center justify-between hover:bg-black-400 p-2 rounded-md h-fit'>
+                        <img className='w-48 h-48 rounded-sm' src={item.images[0].url} alt='playlist cover' />
+                        <h3 className='font-bold mx-auto'>{item.name}</h3>
                     </button>
                 ))}
             </div>
-            :
-            <div>
-                <p>null</p>
-            </div>
         }
-        </>
+        </div>
     )
 }
 
