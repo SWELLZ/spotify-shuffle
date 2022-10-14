cd6969636c6ddac3fc91a43d427b2e64bc00d58a
SWELLZ <microsoftdavid4@gmail.com>
2022-10-13 22:56:05 -0700

    add shuffle feature

--- src/components/shuffleComponents/Songs.jsx
+++ src/components/shuffleComponents/Songs.jsx
@@ -2,6 +2,33 @@ import React, { useEffect, useState } from 'react'
 
 const Songs = ({ playlist, token, toggle }) => {
     const [playlistDetails, setPlaylistDetails] = useState([]);
+    let originalPlaylist = [];
+
+    const confirmChanges = async () => {
+        console.log(playlist)
+        const requestOptions = {
+            method: 'DELETE',
+            headers: {'Authorization': 'Bearer ' + token}
+        }
+        await fetch(`https://api.spotify.com/v1/playlists/${playlist.id}/tracks/1.1`, requestOptions)
+    }
+
+    const shufflePlaylist = () => {
+        var list = playlistDetails
+        var len = list.length;
+        var i = -1;
+        var j, k;
+
+        while (++i < len){ //loops over list until completely shuffled
+            j = Math.floor(Math.random() * len);
+            k = Math.floor(Math.random() * len);
+            var t = list[j];
+            list[j] = list[k];
+            list[k] = t;
+        }
+        setPlaylistDetails([...list]);
+        originalPlaylist = list
+    }
 
     const fetchPlaylistItems = async () => {
         let items = [];
@@ -17,6 +44,7 @@ const Songs = ({ playlist, token, toggle }) => {
             offset += 100;
         } while (items.length < count);
         setPlaylistDetails(items)
+        originalPlaylist = items
     }
 
     useEffect(() => {
@@ -25,8 +53,15 @@ const Songs = ({ playlist, token, toggle }) => {
 
     return (
         <div className='min-h-screen bg-black-500 text-white p-5 relative'>
-            <p className='text-center'>Here is your playlist as it is, if you would like to randomize it, click the button below</p>
-            <button className='bg-green-500 p-4 rounded-full w-1/6 text-black-500 font-bold hover:bg-green-600 mt-3 text-center mx-auto'>SHUFFLE</button>
+            <p className='text-center'>Here is your playlist as it is, if you would like to randomize it, click the button below. When you're happy with your changes, click confirm changes to rearrange the playlist in Spotify</p>
+            <div className='flex justify-center content-center my-3 gap-4'>
+                <button onClick={shufflePlaylist} className='bg-green-500 transition-all duration-300 p-3 rounded-full w-1/6 text-black-500 font-bold hover:bg-green-600 text-center'>SHUFFLE</button>
+                <button onClick={confirmChanges} className='p-3 rounded-full w-1/6 text-black-300 transition-all duration-300 font-bold text-center border-2 border-black-300 hover:bg-black-300 hover:text-white'>Confirm Changes</button>
+                <button onClick={() => toggle(null)} className='underline hover:no-underline'>Cancel</button>
+            </div>
+            <div className='flex justify-center content-center my-3 gap-4'>
+                
+            </div>
             <h1 className='text-center text-2xl font-bold my-2'>{playlist.name}</h1>
             <div>
                 <table className='mx-auto w-full'>
