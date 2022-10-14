2534dec7e29bd7fc9b675bfd1eae45d0813b694f
SWELLZ <microsoftdavid4@gmail.com>
2022-10-14 13:25:04 -0700

    text align center

--- src/components/feedbackComponents/Feedback.jsx
+++ src/components/feedbackComponents/Feedback.jsx
@@ -4,7 +4,7 @@ const Feedback = ({toggle}) => {
   return (
     <div className='absolute mx-auto left-0 right-0 top-0 w-full h-full bg-black-500/50 flex justify-center items-center'>
         <div className='w-4/5 h-1/2 rounded-lg bg-black-500 lg:w-1/2 lg:h-1/2 flex flex-col items-center justify-evenly'>
-            <h1 className='text-2xl text-white'>Your playlist has been shuffled!</h1>
+            <h1 className='text-2xl text-center text-white'>Your playlist has been shuffled!</h1>
             <button onClick={toggle} className='bg-green-500 w-3/4 mx-auto p-5 rounded-full font-bold'>Thank you</button>
         </div>
     </div>
